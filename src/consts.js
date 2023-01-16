bea76a98815f81f8cc8e82f17f3834c49c12fc6f
daiwanxing <377099119@qq.com>
2023-01-16 15:05:55 +0800

    chore: repleace readonly token

--- src/consts.js
+++ src/consts.js
@@ -1,2 +1,2 @@
 // this token can only be used get user info. It's readonly token.
-export const ACCESS_TOKEN = "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";
+export const ACCESS_TOKEN = "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";
